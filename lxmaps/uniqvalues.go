b29c5cdb26a7ddc975214439dfde267ad3a33f96
Hai Nguyen <nthanhhai2909@gmail.com>
2026-03-28 00:17:33 +0700

    docs(lxmaps): enhance UniqValues documentation with nil handling and usage examples

--- lxmaps/uniqvalues.go
+++ lxmaps/uniqvalues.go
@@ -1,6 +1,15 @@
 package lxmaps
 
 // UniqValues returns a slice of unique values from a map.
+// If the maps are nil, it returns an empty slice.
+//
+// Example:
+//
+//	m := map[string]int{"a": 1, "b": 2, "c": 3}
+//	out := UniqValues(m)
+//	// out: []int{1, 2, 3}
+//
+
 func UniqValues[K comparable, V comparable](in ...map[K]V) []V {
 	if len(in) == 0 {
 		return nil
